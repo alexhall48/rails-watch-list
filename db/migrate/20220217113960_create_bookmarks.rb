8b08d7f8da276fac87bf1d570d62cf753d6ddfeb
alex hall <alex_hall@hotmail.co.uk>
2022-02-17 12:42:37 +0000

    models

---
+++ db/migrate/20220217113960_create_bookmarks.rb
@@ -0,0 +1,12 @@
+class CreateBookmarks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bookmarks do |t|
+
+      t.string :comment
+      t.references :movie, foreign_key: true, null: false
+      t.references :list, foreign_key: true, null: false
+
+      t.timestamps
+    end
+  end
+end
